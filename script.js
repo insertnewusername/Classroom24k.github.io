d4427f5f47335ee2d479af6485a4282225c27c0b
IMMAHUMAN <yuchenliu.matthew@gmail.com>
2026-03-21 11:05:11 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,32 @@
+// --- UNIVERSAL NAVIGATION GENERATOR ---
 function generateNav() {
     const nav = document.querySelector('nav');
     if (!nav) return;
 
-    nav.innerHTML = `
+    // "Popular" now leads to popular.html
+    const categories = [
+        { name: "Popular", link: "popular.html" },
+        { name: "Driving", link: "index.html" },
+        { name: "Multiplayer", link: "index.html" },
+        { name: "Sport", link: "index.html" }
+    ];
+
+    let navHTML = `
         <div class="search-container">
             <input type="text" id="gameSearch" placeholder="Search + Enter..." onkeydown="if(event.key==='Enter') filterGames()">
         </div>
         <div class="nav-links">
-            <a href="index.html">Popular</a>
-            <a href="index.html">Driving</a>
-            <a href="index.html">Multiplayer</a>
-            <a href="index.html">Sport</a>
-        </div>
     `;
+
+    categories.forEach(cat => {
+        navHTML += `<a href="${cat.link}">${cat.name}</a>`;
+    });
+
+    navHTML += `</div>`;
+    nav.innerHTML = navHTML;
 }
 
+// --- SEARCH FILTER LOGIC (Triggered by Enter) ---
 function filterGames() {
     let input = document.getElementById('gameSearch').value.toLowerCase();
     let cards = document.getElementsByClassName('game-card');
@@ -24,6 +36,7 @@ function filterGames() {
     }
 }
 
+// --- GAME LOADING LOGIC ---
 function setupGame(gameUrl) {
     const container = document.getElementById('game-container');
     container.innerHTML = `
